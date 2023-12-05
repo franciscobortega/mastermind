65619d750c354bd040bf6de51d0c90ce15c34704
Bryan Ortega <fbryanortega@gmail.com>
2023-12-05 17:25:27 -0600

    Finished logic for check_guess function

--- game.py
+++ game.py
@@ -4,7 +4,22 @@ def generate_code():
     return [1,2,3,4]
 
 def check_guess(secret_code, guess):
-    pass
+    correct_numbers = 0
+    correct_locations = 0
+
+    for index, num in enumerate(guess):
+        if num in secret_code:
+            correct_numbers += 1
+
+        if num == secret_code[index]:
+            correct_locations += 1
+
+    # Did the player guess any number correctly?
+    if correct_numbers == 0 and correct_locations == 0:
+        return "all incorrect"
+    # Are any numbers in the correct location?
+    else:
+        return f"{correct_numbers} correct numbers and {correct_locations} correct locations"
 
 def main():
     SECRET_CODE = generate_code()
@@ -13,7 +28,7 @@ def main():
     print("Welcome to Mastermind!")
     print(f"You have {NUM_ATTEMPTS} attempts to guess the 4-digit code (0-7). Good luck!", SECRET_CODE) # code visible for debugging
 
-    if NUM_ATTEMPTS != 0:
+    while NUM_ATTEMPTS != 0:
         guess = input(f"Enter your guess (e.g., 0123): ")
 
         # convert guess to a list
@@ -22,11 +37,12 @@ def main():
         if guess == SECRET_CODE:
             print("You won! You guessed the code correctly!")
             return
-        
-        # evaluate the guess and provide feedback
-        feedback = check_guess(SECRET_CODE, guess)
-        print(f"Feedback: {feedback}")
+        else:
+            # evaluate the guess and provide feedback
+            feedback = check_guess(SECRET_CODE, guess)
+            print(f"Feedback: {feedback}")
 
+        NUM_ATTEMPTS -= 1
     else:
         print(f"Game over! The secret code was: {SECRET_CODE}")
 
