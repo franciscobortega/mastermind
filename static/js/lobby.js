1412de6c465a3e4ad157af4f618422e5c3dcecc2
Bryan Ortega <fbryanortega@gmail.com>
2023-12-11 01:57:56 -0600

    Added container and input for lobby chat messaging

--- static/js/lobby.js
+++ static/js/lobby.js
@@ -2,49 +2,53 @@
 
 const socket = io();
 
-socket.on("connect", () => {
-  console.log("Connected to WebSocket");
-});
+const sendMessage = () => {
+  console.log("sent");
+};
 
-socket.on("disconnect", () => {
-  console.log("Disconnected from WebSocket");
-});
+// socket.on("connect", () => {
+//   console.log("Connected to WebSocket");
+// });
 
-// Testing unique identifiers
-let uniqueUserId;
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from WebSocket");
+// });
 
-socket.on("user_connected", function (data) {
-  uniqueUserId = data.user_id;
-});
+// // Testing unique identifiers
+// let uniqueUserId;
 
-document.getElementById("lobby-form").addEventListener("submit", function (e) {
-  e.preventDefault();
+// socket.on("user_connected", function (data) {
+//   uniqueUserId = data.user_id;
+// });
 
-  // Extract message from input field
-  const messageInput = document.getElementById("testmessage-input");
-  const message = messageInput.value;
+// document.getElementById("lobby-form").addEventListener("submit", function (e) {
+//   e.preventDefault();
 
-  // Send the message with unique ID to the backend
-  socket.emit("lobby_message", { message, user_id: uniqueUserId });
+//   // Extract message from input field
+//   const messageInput = document.getElementById("testmessage-input");
+//   const message = messageInput.value;
 
-  testMessaging(message, "Me");
-  // Clear the input field
-  messageInput.value = "";
-});
+//   // Send the message with unique ID to the backend
+//   socket.emit("lobby_message", { message, user_id: uniqueUserId });
 
-// WebSocket event for received messages
-socket.on("lobby_message", function (data) {
-  const { message, user_id } = data;
+//   testMessaging(message, "Me");
+//   // Clear the input field
+//   messageInput.value = "";
+// });
 
-  testMessaging(message.message, user_id);
-});
+// // WebSocket event for received messages
+// socket.on("lobby_message", function (data) {
+//   const { message, user_id } = data;
 
-function testMessaging(message, user_id) {
-  // Create a new element for the message
-  let newMessage = document.createElement("p");
-  newMessage.textContent = `${user_id}: ${message}`;
+//   testMessaging(message.message, user_id);
+// });
 
-  // Append the new message
-  let messageContainer = document.querySelector("#lobby-form");
-  messageContainer.appendChild(newMessage);
-}
+// function testMessaging(message, user_id) {
+//   // Create a new element for the message
+//   let newMessage = document.createElement("p");
+//   newMessage.textContent = `${user_id}: ${message}`;
+
+//   // Append the new message
+//   let messageContainer = document.querySelector("#lobby-form");
+//   messageContainer.appendChild(newMessage);
+// }
