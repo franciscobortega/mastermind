e7bd4b01c4ac0da0cc3fb33f291d8e569e3006bb
Bryan Ortega <fbryanortega@gmail.com>
2023-12-12 12:20:00 -0600

    Added tests to validate correct number count and correct location count

--- tests.py
+++ tests.py
@@ -22,6 +22,42 @@ class TestCheckGuess(unittest.TestCase):
         result = check_guess(secret_code, guess)
         self.assertEqual(result, "4 correct numbers and 0 correct locations")
 
+    def test_guess_duplicate_correct_numbers_no_locations(self):
+        secret_code = [0, 0, 1, 1]
+        guess = [1, 1, 2, 3]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "2 correct numbers and 0 correct locations")
+
+    def test_guess_duplicate_correct_numbers_with_locations(self):
+        secret_code = [0, 0, 1, 1]
+        guess = [2, 3, 1, 1]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "2 correct numbers and 2 correct locations")
+
+    def test_guess_one_correct_number_no_locations(self):
+        secret_code = [0, 1, 1, 1]
+        guess = [1, 2, 3, 4]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "1 correct numbers and 0 correct locations")
+
+    def test_guess_one_correct_number_one_location(self):
+        secret_code = [0, 1, 1, 1]
+        guess = [4, 1, 2, 3]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "1 correct numbers and 1 correct locations")
+
+    def test_guess_three_correct_number_two_location(self):
+        secret_code = [0, 1, 1, 1]
+        guess = [1, 1, 1, 2]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "3 correct numbers and 2 correct locations")
+
+    def test_guess_three_correct_number_two_location(self):
+        secret_code = [0, 1, 1, 1]
+        guess = [1, 1, 1, 1]
+        result = check_guess(secret_code, guess)
+        self.assertEqual(result, "3 correct numbers and 3 correct locations")
+
     # TODO: This test causes the rest to fail, likely side effect of using global variables. Consider changing check_guess function to not rely on global variables.
     # def test_guess_all_correct(self):
     #     secret_code = [1, 2, 3, 4]
