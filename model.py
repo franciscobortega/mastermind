a1c1c7dd529ce5688bb8e52b3d295405bf3b7074
Bryan Ortega <fbryanortega@gmail.com>
2023-12-12 01:26:22 -0600

    Added a User SQLAlchemy model

--- model.py
+++ model.py
@@ -4,6 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True, )
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    wins = db.Column(db.Integer, default=0)
+    losses = db.Column(db.Integer, default=0)
+    total_games = db.Column(db.Integer, default=0)
+    # TODO: implement scoring system
+    # total_points = db.Column(db.Integer, default=0)
+
+    routes = db.relationship("Route", back_populates="user")
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} username={self.username}>'
+    
+
 def connect_to_db(flask_app, db_uri="postgresql:///mastermind", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
